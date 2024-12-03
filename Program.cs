68d5237f2788dd034000bcfa91240e79195a3857
jorrtayl <jorrtayl@ut.utm.edu>
2024-12-03 14:34:50 -0600

    Initializing Repo

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        fileName = "input.txt";
+        var data = File.ReadLines(fileName);
+        Console.WriteLine(data);
+        
+        while(isSafe) {
+
+        }
+    }
+
+    static isSafe() {
+
+    }
+}
